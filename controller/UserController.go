49c829ff330b8671b773bdd84b34244db3374289
Yingzhixin <yingzhixin@qq.com>
2021-01-31 19:49:37 +0800

    增加用户登陆功能

--- controller/UserController.go
+++ controller/UserController.go
@@ -7,11 +7,13 @@ import (
 	"github.com/yingzhixin/demo/common"
 	"github.com/yingzhixin/demo/models"
 	"github.com/yingzhixin/demo/util"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 //Register 注册用户
 func Register(ctx *gin.Context) {
+
 	DB := common.GetDB()
 	//获取参数
 	name := ctx.PostForm("name")
@@ -38,29 +40,93 @@ func Register(ctx *gin.Context) {
 		name = util.RandomString(10)
 	}
 
-	//判断手机号是否存在
+	//判断手机号是否已经被注册
 	if isTelephoneExist(DB, telephone) {
 		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
 			"code": 422,
-			"msg":  "用户已经存在",
+			"msg":  "手机号已经被占用",
 		})
 		return
 	}
 
 	//创建用户
+	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 500,
+			"msg":  "密码加密错误",
+		})
+		return
+	}
 	newUser := models.User{
 		Name:      name,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hasedPassword),
 	}
 	DB.Create(&newUser)
 
 	//返回结果
 	ctx.JSON(http.StatusOK, gin.H{
+		"code":    200,
 		"message": "注册成功",
 	})
 }
 
+//Login 用户登陆
+func Login(ctx *gin.Context) {
+	DB := common.GetDB()
+	//获取参数
+	telephone := ctx.PostForm("telephone")
+	password := ctx.PostForm("password")
+
+	//数据验证
+	if len(telephone) != 11 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "手机号必须为11位",
+		})
+		return
+	}
+	if len(password) < 6 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "密码不能少于6位",
+		})
+		return
+	}
+
+	//判断手机号是否存在
+	var user models.User
+	DB.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "用户不存在",
+		})
+		return
+	}
+
+	//判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 400,
+			"msg":  "密码错误",
+		})
+		return
+	}
+
+	//发放token给前端
+	token := "111"
+
+	//返回结果
+	ctx.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": gin.H{"token": token},
+		"msg":  "登陆成功",
+	})
+}
+
+//isTelephoneExist 检测手机号码是否存在
 func isTelephoneExist(db *gorm.DB, telephone string) bool {
 	var user models.User
 	db.Where("telephone = ?", telephone).First(&user)
--- routes.go
+++ routes.go
@@ -8,5 +8,6 @@ import (
 //CollectRoute 处理http
 func CollectRoute(r *gin.Engine) *gin.Engine {
 	r.POST("/api/auth/register", controller.Register)
+	r.POST("/api/auth/login", controller.Login)
 	return r
 }
